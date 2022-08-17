d62f0f092006b89ffe5872ce63550c5b94a34cca
Yasar195 <imyasar07@gmail.com>
2022-08-17 09:04:12 +0530

    changes in design

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -5,17 +5,6 @@ import '../styles/Nav.css';
 const Nav = () => {
     return(
         <div className="nav">
-            <div className="links">
-                <div className="lf">
-                    <h4>Home</h4>
-                </div>
-                <div className="rg">
-                    <h4>Products</h4>
-                    <h4>About</h4>
-                    <h4>Careers</h4>
-                    <h4>Contact</h4>
-                </div>
-            </div>
             <div className="cntr">
                 <img id="logo" src={logo} alt="logo"/>
                 <h1>BETTER FEEDS</h1>
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -6,6 +6,17 @@ const Home = () => {
     return(
         <div className='home'>
             <Nav/>
+            <div className='main'>
+                <div className='about'>
+                    <div className='text'>
+                        <h1>Who we are?</h1>
+                        <p>We are kerala based cattle feed manufacturers</p>
+                    </div>
+                    <div className='image'>
+
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
