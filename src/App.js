2c3ba39af8f58ff629cbdaa97d9650d16dc33dc3
Yasar195 <imyasar07@gmail.com>
2022-08-28 18:40:03 +0530

    added loading animations

--- src/App.js
+++ src/App.js
@@ -4,18 +4,43 @@ import About from './pages/About';
 import Products from './pages/Products';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import ScrollToTop from './Scroll';
+import { ClipLoader } from 'react-spinners';
+import { useEffect, useState } from 'react';
 
 function App() {
+
+  const [loading, setLoading] = useState(false);
+
+  useEffect(()=> {
+    setLoading(true);
+    setTimeout(()=> {
+      setLoading(false);
+    }, 5000)
+  }, [])
+
   return (
-    <BrowserRouter>
-      <ScrollToTop>
-        <Routes>
-          <Route path="/" element={<Home/>}/>
-          <Route path="/about/" element={<About/>}/>
-          <Route path="/products" element={<Products/>}/>
-        </Routes>
-      </ScrollToTop>
-    </BrowserRouter>
+    <div>
+      {
+        loading ?
+        <div className='loading'>
+          <ClipLoader
+            color={'#00A851'}
+            loading={loading}
+            size={100}
+          />
+        </div>
+        :
+        <BrowserRouter>
+        <ScrollToTop>
+          <Routes>
+            <Route path="/" element={<Home/>}/>
+            <Route path="/about/" element={<About/>}/>
+            <Route path="/products" element={<Products/>}/>
+          </Routes>
+        </ScrollToTop>
+      </BrowserRouter>
+      }
+    </div>
   );
 }
 
